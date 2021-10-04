6e43126af57252190da45646604090cb63db0907
krzycho2 <kkrupinski2@gmail.com>
2021-10-04 22:24:04 +0200

    update repo

---
+++ sumo/others/test_net_centrum/build.bat
@@ -0,0 +1,3 @@
+#!/bin/bash
+python "$SUMO_HOME/tools/randomTrips.py" -n osm.net.xml --fringe-factor 5 -p 5.544915 -o osm.passenger.trips.xml -e 3600 --vehicle-class passenger --vclass passenger --prefix veh --min-distance 300 --trip-attributes "departLane=\"best\"" --fringe-start-attributes "departSpeed=\"max\"" --allow-fringe.min-length 1000 --lanes --validate
+python "$SUMO_HOME/tools/randomTrips.py" -n osm.net.xml --fringe-factor 5 -p 21.960020 -o osm.bus.trips.xml -e 3600 --vehicle-class bus --vclass bus --prefix bus --min-distance 600 --fringe-start-attributes "departSpeed=\"max\"" --trip-attributes "departLane=\"best\"" --validate
---
+++ sumo/run_sim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sumo -c net_s8/osm.sumocfg --fcd-output ../outputs/net_s8.xml --fcd-output.attributes id,x,y \
+-b 0 -e 3600 --step-length 0.1 
+python3 parse.py outputs/net_s8.xml
+
+# sumo-gui -c net_s8/osm.sumocfg -b 0 -e 3000 --step-length 0.1 
